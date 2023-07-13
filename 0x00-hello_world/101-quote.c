7bdebecbebad4cfd91c099bb5776e4f188d39ef3
youssefhj <hajibyoussef02@gmail.com>
2023-07-13 16:22:15 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,6 +7,7 @@
  */
 int main(void)
 {
-	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	write(2,s,sizeof(s));
 	return (1);
 }
