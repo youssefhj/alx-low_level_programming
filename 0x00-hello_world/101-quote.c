68d63fd51c7ff458abfdd9b8e0f04b82cd86bbce
youssefhj <hajibyoussef02@gmail.com>
2023-07-13 16:29:47 -0700

    task101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-	write(2,s,strlen(s));
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, s, strlen(s));
 	return (1);
 }
