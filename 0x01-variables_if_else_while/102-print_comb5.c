9c5d0524058df4b6622eaa79e83085e9c6b45dea
youssefhj <hajibyoussef02@gmail.com>
2023-07-14 11:52:22 -0700

    task102

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i, j, k, l, ok;
+
+
+	ok = 0;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			if (j <= i)
+				continue;
+
+			for (k = 0 ; k <= 9 ; k++)
+			{
+				if (k <= j)
+					continue;
+				for (l = 0 ; l <= 9 ; l++)
+				{
+					if (l <= k)
+						continue;
+					if (ok)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					ok = 1;
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
