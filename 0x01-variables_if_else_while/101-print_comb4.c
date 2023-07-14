fb2b4825241114eb8b52cb0ac3b26a3f85c397f2
youssefhj <hajibyoussef02@gmail.com>
2023-07-14 11:37:28 -0700

    task101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int i, j, k, ok;
+
+
+	ok = 0;
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 9 ; j++)
+		{
+			if (j == i)
+				continue;
+
+			for (k = 0 ; k <= 9 ; k++)
+			{
+				if (k <= j)
+					continue;
+
+				if (ok)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0')
+				ok = 1;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
