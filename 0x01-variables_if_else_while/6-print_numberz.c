de5d3db68de79fa0763cfae24f594bdef584a80d
youssefhj <hajibyoussef02@gmail.com>
2023-07-14 09:37:29 -0700

    task6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+		putchar(c + '0');
+
+	putchar('\n');
+	return (0);
+}
