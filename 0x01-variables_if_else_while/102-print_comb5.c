861aa4dc0270dea1c4275ed95b8f91b45ae0e292
youssefhj <hajibyoussef02@gmail.com>
2023-07-14 13:33:29 -0700

    task102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,15 +14,11 @@ int main(void)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			if (j < i)
-				continue;
 			for (k = 0 ; k <= 9 ; k++)
 			{
-				if (k < j)
-				       	continue;
 				for (l = 0 ; l <= 9 ; l++)
 				{
-					if (l < k || ( l == k  && k == j && j == i))
+					if (i > l &&  i == j && j == k)
 						continue;
 					if (ok)
 					{
