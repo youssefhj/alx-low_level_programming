36dac0fee55dafaf404c087ad4f7e31b50e43698
youssefhj <hajibyoussef02@gmail.com>
2023-07-14 09:26:23 -0700

    task3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 success
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 97 ; c < 123 ; c++)
+		putchar(c);
+	for (c = 65 ; c < 91 ; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
