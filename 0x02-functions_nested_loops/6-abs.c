a4a414b001d6a42d45cacc8fac72b609f1ec5998
youssefhj <hajibyoussef02@gmail.com>
2023-07-18 14:12:57 -0700

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value of n
+ * @n: input number
+ * Return: n if positive otherwize -n
+ */
+int _abs(int n)
+{
+	if (n < 0)
+		return (-1 * n);
+
+	return (n);
+}
