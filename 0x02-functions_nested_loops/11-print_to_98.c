9ede60f585903413fa48c16a61b61d3f68ba07d4
youssefhj <hajibyoussef02@gmail.com>
2023-07-18 16:31:46 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,22 +6,25 @@ void print_to_98(int n)
 
 	for (i = n ; i <= 98 ; i++)
 	{
-		if(i < 98)
+		k = i;
+		if( i < -9 || i > 9)
 		{
-			k = i;
-			if( i > 10)
-			{
-				k = i % 10;
-				l = i - k;
-				l = l / 10;
+			if(i < 0)
+				k *= -1;
+				
+			k = k % 10;
+			l = i - k;
+			l = l / 10;
 
-				_putchar(l + '0');
-			}
-
-			_putchar(k + '0');
+			_putchar(l + '0');
+		}
+		_putchar(k + '0');
+		if (i < 98)
+		{
 			_putchar(',');
-			_putchar(' ')
+			_putchar(' ');
 		}
 	}
+	_putchar('\n');
 
 }
