7c4306de01c9385e643356f5cbbe14b2aeb47eac
youssefhj <hajibyoussef02@gmail.com>
2023-07-18 15:41:40 -0700

    task8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,41 +8,22 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0 ; i <= 23 ; i++)
+	for (i = 0 ; i <= 2 ; i++)
 	{
-		for (j = 0 ; j <= 59 ; j++)
+		for (j = 0 ; j <= 3 ; j++)
 		{
-			if (i <= 9)
+			for (k = 0 ; k <= 9 ; k++)
 			{
-				_putchar('0');
-				_putchar(i + '0');
+				for (l = 0 ; l <= 9 ; l++)
+				{
+					_putchar(i + '0');
+					_putchar(j + '0');
+					_putchar(':');
+					_putchar(k + '0');
+					_putchar(l + '0');
+					_putchar('\n');
+				}
 			}
-			else
-			{
-				k = i % 10;
-				l = i / k;
-				l = l % 10;
-
-				_putchar(l + '0');
-				_putchar(k + '0');
-			}
-			_putchar(':');
-		
-			if (j <= 9)
-			{
-				_putchar('0');
-				_putchar(j + '0');
-			}
-			else
-			{
-				k = j % 10;
-				l = j / k;
-				l = l % 10;
-
-				_putchar(l + '0');
-				_putchar(k + '0');
-			}
-			_putchar('\n');
 		}
 	}
 }
