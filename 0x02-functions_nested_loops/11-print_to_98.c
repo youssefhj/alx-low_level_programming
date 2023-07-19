7f864583080dd3fb90150a5741f937faeabe1602
youssefhj <hajibyoussef02@gmail.com>
2023-07-18 17:12:35 -0700

    task11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,29 +1,55 @@
 #include "main.h"
 
+/**
+ * diplay_number - display
+ * @n: input number
+ */
+void display_number(int n)
+{
+	if (n < 9)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	dipsay_number((n - (n % 10)) / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * print_to_98 - print number from n to 98
+ * @n: input number
+ */
 void print_to_98(int n)
 {
-	int i, k, l;
-
-	for (i = n ; i <= 98 ; i++)
+	int i, tmp;
+	
+	i = n;
+	while (1)
 	{
-		k = i;
-		if( i < -9 || i > 9)
-		{
-			if(i < 0)
-				k *= -1;
-				
-			k = k % 10;
-			l = i - k;
-			l = l / 10;
 
-			_putchar(l + '0');
+		if ( i < 0)
+		{
+			tmp = -i;
+			_putchar('-');
 		}
-		_putchar(k + '0');
-		if (i < 98)
+
+		// putchar the number 
+		display_number(tmp);	
+		
+		if (i != 98)
 		{
 			_putchar(',');
-			_putchar(' ');
+			_putchar(' ');		
 		}
+			
+
+		if (i == 98)
+			break;
+
+		if (n > 98)
+			i--;
+		else
+			i++;
 	}
 	_putchar('\n');
 
