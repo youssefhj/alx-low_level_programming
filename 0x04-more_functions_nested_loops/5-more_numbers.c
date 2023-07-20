bb29dca4658c0d35b4521a9253955d7ecbc8d364
youssefhj <hajibyoussef02@gmail.com>
2023-07-20 15:46:25 -0700

    task5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prinft 0 - 14
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			_putchar(j / 10);
+			_putchar(j % 10);
+		}
+		_putchar('\n');
+	}
+
+}
