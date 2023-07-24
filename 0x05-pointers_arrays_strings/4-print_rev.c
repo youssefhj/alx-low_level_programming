5ec95bb744facb295146f581a856c4ad00b83b32
youssefhj <hajibyoussef02@gmail.com>
2023-07-24 16:50:40 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse of s
+ * @s: pointer to string
+ */
+
+void print_rev(char *s)
+{
+	char *ps;
+	int len;
+
+	for (ps = s + _strlen(s) - 1 ; ps >= s ; ps--)
+		_putchar(*ps);
+
+	_putchar('\n');
+
+
+}
