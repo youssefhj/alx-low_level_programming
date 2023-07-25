eb5885d225f593e95441e84baccc6bac80f28d0a
youssefhj <hajibyoussef02@gmail.com>
2023-07-24 18:08:13 -0700

    task8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_num_rec - print number recursive
+ * @n: input
+ */
+void print_num_rec(int n)
+{
+	if (n != 0)
+	{
+		n = n % 10;
+		_putchar(n + '0');
+		print_num_rec(n / 10);
+	}
+}
+
+/**
+ * print_array - display the n element of a
+ * @a: pointer to array of int
+ * @n: size
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		if (a[i] < 0)
+		{
+			_putchar('-');
+			a[i] *= -1;
+		}
+
+		print_num_rec(a[i]);
+
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putcahr(' ');
+		}
+	}
+	_putchar('\n');
+
+}
