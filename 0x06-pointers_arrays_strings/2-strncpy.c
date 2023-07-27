61b838c4b62201fd531ac4a43153a33faf597d95
youssefhj <hajibyoussef02@gmail.com>
2023-07-27 13:41:57 -0700

    task2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strncpy - copy src to dest 
+ * _strncpy - copy src to dest
  * @dest: pointer
  * @src: pointer
  * @n: size
@@ -10,7 +10,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
-	
+
 	for (i = 0 ; i < n ; i++)
 		dest[i] = src[i];
 
