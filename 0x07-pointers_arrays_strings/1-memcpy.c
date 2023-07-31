5ac58cb29aef89c86bd6d70e423036ff2ded6bbb
youssefhj <hajibyoussef02@gmail.com>
2023-07-31 16:54:02 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,14 +4,15 @@
  * _memcpy - copy n byte from src to dest
  * @dest: destination string
  * @src: source string
+ * @n: n bytes
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n ; i++)
 		dest[i] = src[i];
-	
+
 	return (dest);
 }
