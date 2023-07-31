407f07546c7039b6c99a6179d10ed26c70b58904
youssefhj <hajibyoussef02@gmail.com>
2023-07-31 15:43:39 -0700

    task0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _memset - fill s with n byte from b
+ * @s: string
+ * @b: caracter
+ * @n: n byte
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ps;
+
+	for (ps = s ; *ps != '\0' && n > 0 ; ps++, n--)
+		*ps = b;
+
+	return (s);
+}
