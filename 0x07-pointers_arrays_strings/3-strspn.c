c73835938d47d81e68ad181c2b02ca79d3fc9c06
youssefhj <hajibyoussef02@gmail.com>
2023-07-31 17:51:06 -0700

    task3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
+/**
+ * _strspn - count the caracter on s that not exist in accept
+ * @s: string
+ * @accept: string
+ * Return: number
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int i, j, count;
+	unsigned int i, j, count, ok;
 
 	count = 0;
-	for (i = 0 ; accept[i] != 0 ; i++)
-		for (j = 0 ; s[j] != 0 ; j++)
-			if (s[j] == accept[i]`)
-				s[j] = ' ';
-
-	for(i = 0 ; s[i] != ; i++)
-		if (s[i] != ' '
+	for (i = 0 ; s[i] != 0 ; i++)
+	{
+		ok = 1;
+		for (j = 0 ; accept[j] != 0 ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				ok = 0;
+				break;
+			}
+		}
+		if (ok == 1)
+			count++;
+	}
 }
