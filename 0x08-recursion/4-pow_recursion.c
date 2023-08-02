9db3c5d87f21bea160b39fc9268318cbbeda76ef
youssefhj <hajibyoussef02@gmail.com>
2023-08-02 16:45:07 -0700

    task4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,8 +10,8 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else if (!y)
-		return (0);
+	if (y == 0)
+		return (1);
 
 	return (x * _pow_recursion(x, y - 1));
 
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return 
+}
