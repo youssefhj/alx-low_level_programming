35c47787826ac01b1c8511032141704a4d373594
youssefhj <hajibyoussef02@gmail.com>
2023-08-02 14:17:39 -0700

    task1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - display reverse string with recursion
+ * @s: string
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_puts_recursion(++s);
+		_putchar(*s);
+	}
+
+}
