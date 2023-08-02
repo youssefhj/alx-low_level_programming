b568f016f0d9a631c520cf889658af36dc177075
youssefhj <hajibyoussef02@gmail.com>
2023-08-02 13:38:12 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,11 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-		return (_putchar('\n'));
-
+	{
+		_putchar('\n');
+		return ;
+	}
 	_putchar(*s);
-	return (_puts_recursion(s++));
+	_puts_recursion(s++);
 
 }
