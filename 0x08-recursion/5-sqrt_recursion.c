b6dc3cf9ad16a2699a71ec0c2fc1251c391810d3
youssefhj <hajibyoussef02@gmail.com>
2023-08-02 17:19:18 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,9 +1,30 @@
 #include "main.h"
 
-int _sqrt_recursion(int n)
+/**
+ * check - chek if n natural square
+ * @d: input
+ * @n: input
+ * Return: -1 not natural square ;sqr(n) otherwize
+ */
+int check(int d, int n)
 {
-	if (n < 0)
+	if (d * d == n)
+		return (d);
+
+	if (d == 0 || d == 1)
 		return (-1);
 
-	return 
+	return (check(d / 2, n));
+}
+/**
+ * _sqrt_recursion - sqrt of with recursion
+ * @n: input
+ * Return: sqrt(n)
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0 || (n % 2) != 0)
+		return (-1);
+
+	return (check(n, n));
 }
