936adba5f5ece9f16c7506f8dcc3b957b4a75a78
youssefhj <hajibyoussef02@gmail.com>
2023-08-04 19:55:25 -0700

    task0

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -10,20 +10,20 @@ int _atoi(char *s)
 {
 
 	int signe = 1, i = 0;
-        unsigned int result = 0;
+	unsigned int result = 0;
 
-        while (!(s[i] <= '9' && s[i] >= '0' && s[i] != '\0'))
-        {
-                if (s[i] == '-')
-                        signe *= -1;
-                i++;
-        }
+	while (!(s[i] <= '9' && s[i] >= '0' && s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			signe *= -1;
+		i++;
+	}
 
-        while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
-        {
-                result = (result * 10) + (s[i] - '0');
-                i++;
-        }
-        result *= signe;
-        return (result);
+	while (s[i] <= '9' && s[i] >= '0' && s[i] != '\0')
+	{
+		result = (result * 10) + (s[i] - '0');
+		i++;
+	}
+	result *= signe;
+	return (result);
 }
