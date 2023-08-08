770e360da62bfb2f8c730a428ca1522be16a7db6
youssefhj <hajibyoussef02@gmail.com>
2023-08-08 15:14:08 -0700

    task2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - string concatenation
+ * @s1: first string
+ * @s2: second string
+ * Return: a pointer to the concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *str;
+	int lens1, lens2, i;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+		lens1++;
+	for (i = 0; s2[i] != '\0' ; i++
+			lens2++;
+
+	str = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	if (str != NULL)
+	{
+		for (i = 0 ; s1[i] != '\0' ; i++)
+			str[i] = s1[i];
+
+		for (i = 0 ; s2[i] != '\0' ; i++)
+			str[i] = s2[i];
+
+		str[i] = '\0';
+	}
+	return (str);
+}
