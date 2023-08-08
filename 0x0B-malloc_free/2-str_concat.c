75ca10e25c159cbdc69a49881f30cffe8901ac44
youssefhj <hajibyoussef02@gmail.com>
2023-08-08 15:23:34 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,22 +12,25 @@ char *str_concat(char *s1, char *s2)
 	char *str;
 	int lens1, lens2, i;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
 
-	for (i = 0 ; s1[i] != '\0' ; i++)
+	lens1 = lens2 = 0;
+
+	for (i = 0 ; s1 != NULL && s[i] != '\0' ; i++)
 		lens1++;
-	for (i = 0; s2[i] != '\0' ; i++)
-			lens2++;
+
+	for (i = 0; s2 != NULL && s2[i] != '\0' ; i++)
+		lens2++;
 
 	str = malloc(sizeof(char) * (lens1 + lens2 + 1));
 	if (str != NULL)
 	{
-		for (i = 0 ; s1[i] != '\0' ; i++)
+		for (i = 0 ; s1 != NULL && s1[i] != '\0' ; i++)
 			str[i] = s1[i];
 
-		for (i = 0 ; s2[i] != '\0' ; i++)
+		for (i = 0 ;  s2 != NULL && s2[i] != '\0' ; i++)
 			str[i] = s2[i];
 
 		str[i] = '\0';
