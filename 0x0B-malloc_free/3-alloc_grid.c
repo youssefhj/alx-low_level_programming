d056c6ae57dcb577d596a7cf64bd1cc493353a7a
youssefhj <hajibyoussef02@gmail.com>
2023-08-08 16:25:28 -0700

    task3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * alloc_grid - allocate grid
+ * @width: grid column
+ * @height: grid row
+ * Return: grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **matrix, **pmatrix;
+	int l, c;
+
+	matrix = (int **) malloc(sizeof(int **) * height);
+
+	if (matrix == NULL)
+		return (NULL);
+
+	for (pmatrix = matrix ; pmatrix < matrix + height ; pmatrix++)
+		*pmatrix = (int *) malloc(sizeof(int *) * width);
+
+	for (l = 0 ; l < height ; l++)
+		for (c = 0 ; c < width ; c++)
+			matrix[l][c] = 0;
+
+	return (matrix);
+
+}
