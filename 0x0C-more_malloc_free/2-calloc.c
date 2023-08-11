f7840a0b7f6a100d5a93bf9716de721f39e08dc9
youssefhj <hajibyoussef02@gmail.com>
2023-08-11 13:37:27 -0700

    task2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocate memory
+ * @nmemb: number of memmbre
+ * @size: size of each member
+ * Return: pointer to an array of any type
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *vp;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	vp = malloc(size * nmemb);
+	if (vp == NULL)
+		return (NULL);
+
+	return (vp);
+}
