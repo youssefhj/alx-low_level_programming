3085e18d9197eaefe9e0f365f580d403c01daf2d
youssefhj <hajibyoussef02@gmail.com>
2023-08-11 12:34:26 -0700

    task0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory of size b
+ * @b: size
+ * Return: any type of pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	return (malloc(b));
+
+}
