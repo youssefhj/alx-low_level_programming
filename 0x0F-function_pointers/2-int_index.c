87766f57343c99880fd30761cd62ca8eee0cf9f9
youssefhj <hajibyoussef02@gmail.com>
2023-08-16 14:55:33 -0700

    task2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - search for integer
+ * @array: array
+ * @size: size
+ * @cmp: function pointer
+ *
+ * Return: search element in success otherwize -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (0);
+
+	if (array && cmp)
+	{
+		for (i = 0 ; i < size ; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (array[i]);
+
+		}
+	}
+	return (-1)
+}
