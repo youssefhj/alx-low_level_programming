49f8221588d588070b17c229c932d636c76235ed
youssefhj <hajibyoussef02@gmail.com>
2023-08-16 14:04:11 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+
+/**
+ * print_name - print name using f function
+ *
+ * @name: input string
+ * @f: pointer to display function
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,7 @@
+#ifndef _FUNCTION_
+#define _FUNCTION_
+
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
