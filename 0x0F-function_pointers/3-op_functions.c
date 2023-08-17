7c5cf706bb186557b7cd2b3a90f227ae08eb0282
youssefhj <hajibyoussef02@gmail.com>
2023-08-16 17:23:43 -0700

    task3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -46,7 +46,7 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
-	if (b < 0)
+	if (b == 0)
 		return (-1);
 
 	return (a / b);
@@ -61,7 +61,7 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
-	if (b < 0)
+	if (b == 0)
 		return (-1);
 
 	return (a % b);
