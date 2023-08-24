d52a17c71c86173b3ea1189459d49fc23acc71d1
youssefhj <hajibyoussef02@gmail.com>
2023-08-24 15:18:11 -0700

    task2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * add_node - add node at the beginning
+ * @head: double pointer
+ * @str: string
+ *
+ * Return: new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *node, *tmph;
+
+	node = (list_t *)malloc(sizeof(list_t));
+	if (node == NULL)
+		return (*head);
+
+	node->str = strdup(str);
+	node->len = _strlen(node->str);
+	node->next = NULL;
+	
+	tmph = *head;
+	*head = node;
+	node->next = tmph;
+
+	return (node);
+}
