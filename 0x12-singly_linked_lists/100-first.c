b216eaa9a0668ac871b93ea187c69464d19acaf3
youssefhj <hajibyoussef02@gmail.com>
2023-08-24 16:14:07 -0700

    task5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+void _start(void) __attribute__ ((constuctor));
+
+/**
+ * _start - executes before main
+ *
+ * Return: void
+ */
+
+void _start(void)
+{
+	printf("You're beat! and yet, you must allow,\n
+		I bore my house upon my back!\n");
+
+}
