8730ec72a98ddb464a9a48d50477c91314854b62
youssefhj <hajibyoussef02@gmail.com>
2023-08-24 13:54:35 -0700

    task0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,6 +2,24 @@
 #include <stdio.h>
 
 /**
+ * _strlen - string length
+ * @str: string
+ *
+ * Return: length
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int len;
+
+	if (str == NULL)
+		return (0);
+
+	for (len = 0 ; str != '\0' ; len++)
+		;
+
+	return (len);
+}
+/**
  * print_list - display content of the list
  * @h: head node of the list
  *
@@ -12,13 +30,10 @@ size_t print_list(const list_t *h)
 	list_h *ph = NULL;
 	size_t node = 0;
 
-	if (ph->str == NULL)
-		printf("%s\n", "[0] (nil)");
-
 	ph = h;
 	while (ph)
 	{
-		printf("[%lu] %s\n", ph->len, ph->str);
+		printf("[%lu] %s\n", _strlen(ph->str), ph->str ? ph->str : "(nil)");
 		ph = ph->next;
 		node++;
 	}
