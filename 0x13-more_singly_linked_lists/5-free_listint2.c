48ec187b02cd019c48e5cb35ac7d5f318d931b5a
youssefhj <hajibyoussef02@gmail.com>
2023-08-29 16:23:18 -0700

    task5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free the list
+ * @head: first node of the list
+ *
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ph;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (*head)
+	{
+		ph = *head;
+		*head = (*head)->next;
+		free(ph);
+	}
+
+	free(head);
+	head = NULL;
+}
