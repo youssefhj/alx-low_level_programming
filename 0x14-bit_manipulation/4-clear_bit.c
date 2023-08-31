fc82dffc845d064a36fb7f3a9a37d867af2ee847
youssefhj <hajibyoussef02@gmail.com>
2023-08-31 07:36:58 -0700

    task4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * clear_bit - set bit to 0 at ith-index
+ * @n: number
+ * @index: position
+ *
+ * Return: 1 on success -1 on failed
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index < sizeof(*n) * 8)
+	{
+		(*n) &= 1L << index;
+		return (1);
+	}
+
+	return (-1);
+}
