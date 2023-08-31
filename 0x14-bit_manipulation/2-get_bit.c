ca4a207b01d40742f2995371d4c2568e77a7d97e
youssefhj <hajibyoussef02@gmail.com>
2023-08-31 07:00:42 -0700

    task2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -9,15 +9,10 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int bitsize = sizeof(n) * 8;
 
-	if (index <= bitsize)
-	{
-		if ((n >> index) & 1)
-			return (1);
-
-		return (0);
-	}
+	if (index < sizeof(n) * 8)
+		return (n >> index & 1);
 
 	return (-1);
+
 }
