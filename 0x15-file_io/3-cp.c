a51830cde90bf0fa481a5c21bc1c25dac62b415d
youssefhj <hajibyoussef02@gmail.com>
2023-09-04 17:25:10 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -14,7 +14,7 @@ int main(int ac, char *av[])
 
 	if (ac < 3)
 	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
