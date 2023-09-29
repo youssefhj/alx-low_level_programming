3891159e95c83ca85a8a20d2ab73ab9eb7b68d58
youssefhj <hajibyoussef02@gmail.com>
2023-09-28 18:40:43 -0700

    task1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_binary - print n in binary format
+ * @n: number
+ *
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int size = 8 * sizeof(unsigned long int);
+
+	while (size)
+	{
+		if (n & 1)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		n = n >> 1;
+		size--;
+	}
+}
