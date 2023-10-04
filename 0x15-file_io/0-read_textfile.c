b08d8e2e05322c0001ad34c07bcc63aeb5a1320f
youssefhj <hajibyoussef02@gmail.com>
2023-10-03 18:20:30 -0700

    task1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -27,8 +27,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (lenr == -1)
 		return (0);
 
-	lenw = write(1, buf, letters);
-	if (lenw < lenr)
+	lenw = write(1, buf, lenr);
+	if (lenw != lenr)
 		return (0);
 
 	free(buf);
