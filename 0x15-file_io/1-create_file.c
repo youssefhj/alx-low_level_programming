86aaa8cdfb0148bb3cc48ce9bf61fc1b56606251
youssefhj <hajibyoussef02@gmail.com>
2023-10-04 14:36:58 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -37,7 +37,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (fd == -1)
 		return (-1);
-	
+
 	if (text_content != NULL)
 	{
 		len = write(fd, text_content, _strlen(text_content));
