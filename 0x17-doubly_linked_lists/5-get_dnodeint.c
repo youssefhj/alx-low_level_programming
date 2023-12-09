15aa9e81a5cb5bd368b65a977d4fa475da492080
youssefhj <hajibyoussef02@gmail.com>
2023-12-08 16:29:47 -0800

    task5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get node at index
+ * @head: head of the list
+ * @index: position
+ *
+ * Return: node on success NULL on failure
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *p = head;
+	int idx = 0;
+
+	if (head == NULL)
+		return (NULL);
+
+	while (p)
+	{
+		if (idx == index)
+			return (p);
+
+		p = p->next;
+		idx++;
+	}
+
+	return (NULL);
+}
