3fc82566c4b4735b47527d0797a21652d70ea5c6
youssefhj <hajibyoussef02@gmail.com>
2023-12-23 14:15:00 -0800

    task0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *ht;
+	unsigned long int i;
 
 	ht = (hash_table_t *)malloc(sizeof(hash_table_t));
 	if (ht == NULL)
@@ -20,6 +21,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(ht);
 		return (NULL);
 	}
+
+	ht->size = size;
+	for (i = 0 ; i < size; i++)
+		ht->array[i] = NULL;
 
 	return (ht);
 }
