051b9fadd09fd05c61e63d505a98b6ff869a6cd4
youssefhj <hajibyoussef02@gmail.com>
2023-12-24 17:41:23 -0800

    task6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - delete all hash table
+ * @ht: hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int idx;
+	hash_node_t *node;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+
+	for (idx = 0; idx < ht->size; id++)
+	{
+		while (ht->array[idx] != NULL)
+		{
+			node = ht->array[idx]->next;
+			free(ht->array[idx]->key);
+			free(ht->array[idx]->value);
+			free(ht->array[idx]);
+
+			ht->array[idx] = node;
+
+		}
+	}
+	
+	free(ht->array);
+	free(ht);
+}
