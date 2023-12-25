3a2e784f1dd903306f3001d36a89cf69db89ebe0
youssefhj <hajibyoussef02@gmail.com>
2023-12-24 17:08:28 -0800

    task5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -10,22 +10,33 @@ void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int idx;
 	hash_node_t *ptr;
+	int dec;
 
 	if (ht != NULL)
 	{
+		printf("{");
 		for (idx = 0; idx < ht->size; idx++)
 		{
 			ptr = ht->array[idx];
-			printf("{");
+			dec = idx - 1;
 			while (ptr)
 			{
-				printf("\'%s\' : '\%s\'", ptr->key, ptr->value);
+				while (dec >= 0)
+				{
+					if (ht->array[dec] != NULL)
+					{
+						printf(", ");
+						break;
+					}
+					dec--;
+				}
+				printf("\'%s\': \'%s\'", ptr->key, ptr->value);
 				if (ptr->next != NULL)
 					printf(", ");
 
 				ptr = ptr->next;
 			}
-			printf("}");
 		}
+		printf("}\n");
 	}
 }
